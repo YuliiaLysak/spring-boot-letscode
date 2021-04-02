4f03e70f58c5ee01b1f4985d564d765759e86498
Yuliia Lysak <julia.m.lysak@gmail.com>
2021-04-02 21:47:20 +0300

    fixed some bugs - Added bean validation, passwords encoding in database

--- src/main/java/com/example/springbootletscode/service/UserService.java
+++ src/main/java/com/example/springbootletscode/service/UserService.java
@@ -42,8 +42,8 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
-        userRepository.save(user);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
 
         sendMessage(user);
 
